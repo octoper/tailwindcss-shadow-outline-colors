f82d6fb4086d6b2b7b837f8a0a904753b943858e
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2020-03-25 22:49:02 +0200

    Some nice changes

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tailwind CSS Shadow Outline Colors
+
+![license](https://badgen.net/github/license/octoper/tailwindcss-shadow-outline-colors)
+![npm](https://badgen.net/npm/v/tailwindcss-shadow-outline-colors)
+
+## Installation
+
+```bash
+npm install tailwindcss-shadow-outline-colors
+```
+
+or using if you are using `yarn`
+
+```bash
+yarn add tailwindcss-shadow-outline-colors
+```
+
+Add the plugin to the `plugins` array in your Tailwind CSS configuration.
+
+```js
+plugins: [
+    require('tailwindcss-shadow-outline-colors')()
+]
+```
--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const flattenColorPalette = require('./utils/flattenColorPalette')
 
 module.exports = function () {
     return function ({ addUtilities, e, theme, variants }) {
-        const colors = flattenColorPalette(theme('shadowOutlineColors'))
+        const colors = flattenColorPalette(theme('shadowOutlineColors') ? theme('shadowOutlineColors') : theme('colors'))
+        const shadowConf = theme('shadowOutline.shadow') ? theme('shadowOutline.shadow') : '0 0 0 0.125em'
+        const alphaConf = theme('shadowOutline.alpha') ? theme('shadowOutline.alpha') : '0.25'
+
         const utilities = _.fromPairs(
             _.map(_.omit(colors, 'default'), (value, modifier) => {
                 return [
                     `.${e(`shadow-outline-${modifier}`)}`,
                     {
-                        'box-shadow': `0 0 0 3px ${color(value).alpha(0.5).rgb().string()}`,
+                        'box-shadow': `${shadowConf} ${color(value).alpha(alphaConf).rgb().string()}`,
                     },
                 ]
             })
         )
 
-        addUtilities(utilities, variants('shadowOutlineColors'))
+        addUtilities(utilities, variants('shadowOutline'))
     }
 }
