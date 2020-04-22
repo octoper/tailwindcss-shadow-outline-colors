b5635d780cbe0f2c94772a2861f741cb6997e7b8
Vaggelis Yfantis <me@octoper.me>
2020-04-22 12:33:51 +0300

    add currentColor support

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const _ = require('lodash');
-const color = require('color');
+const Color = require('color');
 const flattenColorPalette = require('./utils/flattenColorPalette')
 
 
 module.exports = function () {
-    return function ({ addUtilities, e, theme, variants }) {
+    return function ({
+        addUtilities,
+        e,
+        theme,
+        variants
+    }) {
         const colors = flattenColorPalette(theme('shadowOutlineColors') ? theme('shadowOutlineColors') : theme('colors'))
         const shadowConf = theme('shadowOutline.shadow') ? theme('shadowOutline.shadow') : '0 0 0 0.125em'
         const alphaConf = theme('shadowOutline.alpha') ? theme('shadowOutline.alpha') : '0.25'
 
         const utilities = _.fromPairs(
             _.map(_.omit(colors, 'default'), (value, modifier) => {
+                let color = value;
+                if (value != 'currentColor') {
+                    color = Color(value).alpha(alphaConf).rgb().string()
+                }
                 return [
                     `.${e(`shadow-outline-${modifier}`)}`,
                     {
-                        'box-shadow': `${shadowConf} ${color(value).alpha(alphaConf).rgb().string()}`,
+                        'box-shadow': `${shadowConf} ${color}`,
                     },
                 ]
             })
